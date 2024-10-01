46d353bb73686f3a3ab70d36b01056677004ba01
developer-mixa <mixail_081177@mail.ru>
2024-10-01 23:55:14 +0300

    config: now i use cmake

--- commands/build.sh
+++ commands/build.sh
@@ -1,5 +1,5 @@
 rm -rf ./build
 mkdir -p ./build
-g++ -c src/main.cpp -o build/main.o
-g++ build/main.o -o build/sfml-app -lsfml-graphics -lsfml-window -lsfml-system
-./build/sfml-app
+cd build
+cmake ..
+make && ./PingPong
