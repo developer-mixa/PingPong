4610306c5ac24beee4624047b1dece90676b5ccd
developer-mixa <mixail_081177@mail.ru>
2024-10-02 18:34:30 +0300

    feat: add Play button

--- src/include/StartScreen.hpp
+++ src/include/StartScreen.hpp
@@ -6,12 +6,20 @@
 #include <iostream>
 
 class StartScreen : public MonoBehavior {
-public:
-    void update(sf::RenderWindow& window) override;
-    void start(sf::RenderWindow& window) override;
+private:
+    void initBackground();
+    void initStartButton(sf::RenderWindow& window);
+    void initQuitButton();
     sf::CircleShape shape; 
     sf::Sprite background;
     sf::Texture backgroundTexture;
+    sf::RectangleShape startButton;
+    sf::Text buttonTextStart;
+    sf::Font font;
+
+public:
+    void update(sf::RenderWindow& window) override;
+    void start(sf::RenderWindow& window) override;
 };
 
 #endif
--- src/main/start/StartScreen.cpp
+++ src/main/start/StartScreen.cpp
@@ -3,22 +3,45 @@
 #include <iostream>
 #include "StartScreen.hpp"
 
-void initBackground(){
-    
+#define BUTTON_WIDTH 200
+#define BUTTON_HEIGHT 50
+#define OFFSET_DIVIDER 24
+#define FONT_SIZE 24
+#define FONT_OFFSET 10
+#define PLAY_TEXT "Play"
+#define START_SCREEN_PATH "assets/images/start_screen.jpg"
+#define FONT_PATH "assets/fonts/Roboto-Black.ttf"
+
+void StartScreen::initBackground(){
+
+    backgroundTexture.loadFromFile(START_SCREEN_PATH);
+    background = sf::Sprite(backgroundTexture);
+}
+
+void StartScreen::initStartButton(sf::RenderWindow& window){
+    const auto windowSize = window.getSize();
+    const int offset = windowSize.x / OFFSET_DIVIDER;
+
+    startButton = sf::RectangleShape(sf::Vector2f(BUTTON_WIDTH, BUTTON_HEIGHT));
+    buttonTextStart = sf::Text(PLAY_TEXT, font, FONT_SIZE);
+    startButton.setFillColor(sf::Color::Green);
+    startButton.setOutlineThickness(2);
+    startButton.setOutlineColor(sf::Color::Black);
+    buttonTextStart.setCharacterSize(FONT_SIZE);
+    buttonTextStart.setFillColor(sf::Color::White);
+    startButton.setPosition(windowSize.x-BUTTON_WIDTH-offset, BUTTON_HEIGHT);
+    buttonTextStart.setPosition(startButton.getPosition().x + FONT_OFFSET, startButton.getPosition().y + FONT_OFFSET);
 }
 
 void StartScreen::start(sf::RenderWindow &window){
-    shape = sf::CircleShape(100.f, 3);
-
-    if (!backgroundTexture.loadFromFile("assets/images/start_screen.jpg")) {
-        std::cout << "Ошибка загрузки текстуры фона!" << std::endl;
-        shape = sf::CircleShape(500.f, 3);
-        return;
-    }
-    background = sf::Sprite(backgroundTexture);
+    font.loadFromFile(FONT_PATH);
+    initBackground();
+    initStartButton(window);
     fitToScreen(background, window);
 }
 
 void StartScreen::update(sf::RenderWindow &window){
     window.draw(background);
+    window.draw(startButton);
+    window.draw(buttonTextStart);
 }
