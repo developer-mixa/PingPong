4bdbe4cd66ad53b95ce6d91020716b6049b72ee2
developer-mixa <mixail_081177@mail.ru>
2024-10-04 10:29:39 +0300

    fix: now switching scenes is correct

--- src/include/Engine.hpp
+++ src/include/Engine.hpp
@@ -8,14 +8,16 @@ private:
     std::map<int, Scene> scenes;
     static Engine* instance;
     Scene currentScene;
-    void build();
     Engine(Scene &firstScene);
+    bool isDestroy;
 public:
     static Engine& getInstance();
     static void create(Scene &scene);
     Scene& getActiveScene();
     void setActiveScene(int indexScene);
-    void registerScene(int index, Scene &scene);
+    Engine& registerScene(int index, Scene &scene);
+    void build();
+    void destroy();
 };
 
 #endif
